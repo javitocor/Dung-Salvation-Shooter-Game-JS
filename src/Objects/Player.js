d30dec817b9f420bdecf2577276eadf9de6ddb2f
javitocor <56809963+javitocor@users.noreply.github.com>
2020-07-29 15:52:52 +0200

    Add players and entities class

---
+++ src/Objects/Player.js
@@ -0,0 +1,57 @@
+import Phaser from 'phaser';
+import Entity from './entities';
+
+class Player extends Entity {
+  constructor(scene, x, y, key) {
+    super(scene, x, y, key, "Player");
+
+    this.setData("speed", 200);
+
+    this.setData("isShooting", false);
+    this.setData("timerShootDelay", 10);
+    this.setData("timerShootTick", this.getData("timerShootDelay") - 1);
+  }
+
+  moveUp() {
+    this.body.velocity.y = -this.getData("speed");
+  }
+  moveDown() {
+    this.body.velocity.y = this.getData("speed");
+  }
+  moveLeft() {
+    this.body.velocity.x = -this.getData("speed");
+  }
+  moveRight() {
+    this.body.velocity.x = this.getData("speed");
+  }
+
+  onDestroy() {
+    this.scene.time.addEvent({
+      delay: 1000,
+      callback: function() {
+        this.scene.scene.start("SceneGameOver");
+      },
+      callbackScope: this,
+      loop: false
+    });
+  }
+
+  update() {
+    this.body.setVelocity(0, 0);
+    this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
+    this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+    if (this.getData("isShooting")) {
+      if (this.getData("timerShootTick") < this.getData("timerShootDelay")) {
+        this.setData("timerShootTick", this.getData("timerShootTick") + 1); 
+      }
+      else { 
+        var laser = new PlayerLaser(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+      
+        this.scene.sfx.laser.play(); 
+        this.setData("timerShootTick", 0);
+      }
+    }
+  }
+}
---
+++ src/scrolling.js
@@ -0,0 +1,35 @@
+class ScrollingBackground {
+  constructor(scene, key, velocityY) {
+    this.scene = scene;
+    this.key = key;
+    this.velocityY = velocityY;
+
+    this.layers = this.scene.add.group();
+
+    this.createLayers();
+  }
+
+  createLayers() {
+    for (var i = 0; i < 2; i++) {
+      var layer = this.scene.add.sprite(0, 0, this.key);
+      layer.y = (layer.displayHeight * i);
+      var flipX = Phaser.Math.Between(0, 10) >= 5 ? -1 : 1;
+      var flipY = Phaser.Math.Between(0, 10) >= 5 ? -1 : 1;
+      layer.setScale(flipX * 2, flipY * 2);
+      layer.setDepth(-5 - (i - 1));
+      this.scene.physics.world.enableBody(layer, 0);
+      layer.body.velocity.y = this.velocityY;
+
+      this.layers.add(layer);
+    }
+  }
+
+  update() {
+    if (this.layers.getChildren()[0].y > 0) {
+      for (var i = 0; i < this.layers.getChildren().length; i++) {
+        var layer = this.layers.getChildren()[i];
+        layer.y = (-layer.displayHeight) + (layer.displayHeight * i);
+      }
+    }
+  }
+}
