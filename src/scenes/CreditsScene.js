12e4dc115438ca0227c46fec3faf93483848212e
javitocor <56809963+javitocor@users.noreply.github.com>
2020-07-29 11:20:27 +0200

    Modify credit scene

--- src/scenes/CreditsScene.js
+++ src/scenes/CreditsScene.js
@@ -8,8 +8,11 @@ export default class CreditsScene extends Phaser.Scene {
 
   create () {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Javier Correas', { fontSize: '26px', fill: '#fff' });
+    this.madeByText2 = this.add.text(0, 0, 'Capstone Project for Microverse School', { fontSize: '26px', fill: '#fff' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.skipText = this.add.text(10, 10, 'Press SPACE\nto skip', { fontSize: '10px', fill: '#fff' });
 
     Phaser.Display.Align.In.Center(
       this.creditsText,
@@ -21,7 +24,13 @@ export default class CreditsScene extends Phaser.Scene {
       this.zone
     );
 
+    Phaser.Display.Align.In.Center(
+      this.madeByText2,
+      this.zone
+    );
+
     this.madeByText.setY(1000);
+    this.madeByText.setY(1150);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
@@ -34,16 +43,29 @@ export default class CreditsScene extends Phaser.Scene {
       }
     });
 
-    this.madeByTween = this.tweens.add({
+    this.madeByTween1 = this.tweens.add({
       targets: this.madeByText,
       y: -300,
       ease: 'Power1',
       duration: 8000,
+      delay: 1000
+    });
+
+    this.madeByTween2 = this.tweens.add({
+      targets: this.madeByText2,
+      y: -200,
+      duration: 20000,
       delay: 1000,
       onComplete: function () {
         this.madeByTween.destroy;
         this.scene.start('Title');
       }.bind(this)
     });
+  }
+
+  update() {
+    if (this.keySpace.isDown) {
+      this.scene.start('Title');
+    }
   }
 };
