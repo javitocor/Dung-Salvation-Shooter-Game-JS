25bb1bdb673464eccd6431b633bbbe4954ffe4de
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-02 10:56:56 +0200

    Create getscore function

--- src/helpers/getScore.js
+++ src/helpers/getScore.js
@@ -1,9 +1,11 @@
 const getScore = (key) => {
+  let score = 0;
   if (key === 'space') {
     score = 0;
   } else {
     score = parseInt(JSON.parse(window.localStorage.getItem('score')));
   }
+  return score;
 }
 
 export default getScore;
