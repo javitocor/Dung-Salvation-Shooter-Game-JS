ff6d6c7bf61f36c238d3aa67619691295c1a842e
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-04 07:11:33 +0200

    Fix line in webpack base.js

--- webpack/base.js
+++ webpack/base.js
@@ -15,9 +15,6 @@ module.exports = {
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
-        options: {
-          presets: ['env'],
-        },
       },
     },
     {
