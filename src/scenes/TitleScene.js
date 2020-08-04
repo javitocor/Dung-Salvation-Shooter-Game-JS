8261e7ec7e86ce0f3e97aa43241fed352e3d4f53
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-04 16:42:46 +0200

    Add encode to audio bg

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -7,10 +7,6 @@ export default class TitleScene extends Phaser.Scene {
     super('Title');
   }
 
-  preload() {
-    this.load.audio('bgMusic', 'assets/DaftPunk-Bestof-11Aerodynamic.mp3');
-  }
-
   create() {
     this.add.image(400, 300, 'bg2').setDisplaySize(800, 600);
     this.add.image(this.game.config.width * 0.5, 100, 'logo2');
@@ -18,6 +14,9 @@ export default class TitleScene extends Phaser.Scene {
     this.optionsButton = new Button(this, config.width / 2, config.height / 2, 'blueButton1', 'blueButton2', 'Options', 'Options');
     this.creditsButton = new Button(this, config.width / 2, config.height / 2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
     this.leaderBoardButton = new Button(this, config.width / 2, config.height / 2 + 200, 'blueButton1', 'blueButton2', 'Leaders', 'Leaderboard');
+
+    const audio = this.add.audio('bgMusic');
+    this.sound.setDecodedCallback([audio], this.bgMusic.play, this);
 
     this.model = this.sys.game.globals.model;
     if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
