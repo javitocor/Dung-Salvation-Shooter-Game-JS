4c4a8f4947ff09ae3dfd10715f14fb701e543fe5
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-04 06:48:44 +0200

    Add dependencies to package.json

--- webpack/base.js
+++ webpack/base.js
@@ -15,6 +15,9 @@ module.exports = {
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+        },
       },
     },
     {
